ae892a9cc0382b482ce067db6eab3f27ae98a7e4
Ash <bunkas01@github.noreply.com>
2017-05-09 12:18:24 -0500

    slightly tweaked main page again, by adding an action to the search form. Added the initial version of the search results template, and modified template_tester.py to view it.

--- template_tester.py
+++ template_tester.py
@@ -18,5 +18,15 @@ def main_page():
     return render_template("main_page.html", user=u)
 
 
+@app.route("/search")
+def results_page():
+    u = User()
+    res = [[3, "Ashleigh", "Adelaide the Mercenary"],
+           [1, "Eric", "Phillip the Bastard"],
+           [5, "Ashleigh", "Anna the Knight"],
+           [7, "Anonymous", "Mildred the Surgeon"]]
+    return render_template("search_results.html", user=u, results=res)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
