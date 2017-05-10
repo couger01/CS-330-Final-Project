9942f2e18e88f693768b10661981996b40c5f89f
Ash <bunkas01@github.no-reply.com>
2017-05-09 21:53:23 -0500

    modified test app to run tests on the search results page template, and finished developing aforementioned template. This closes #5

--- template_tester.py
+++ template_tester.py
@@ -7,14 +7,14 @@ Bootstrap(app)
 
 
 class User:
-    def __init__(self, name="Anonymous", logged_in=True):
+    def __init__(self, name="Anonymous", logged_in=False):
         self.name = name
         self.logged_in = logged_in
 
 
 @app.route("/")
 def main_page():
-    u = User()
+    u = User(logged_in=True)
     upstr = """
             Lorem ipsum dolor sit amet, consectetur adipiscing
             elit. Vivamus eu odio sit amet nibh consequat
@@ -33,24 +33,11 @@ def main_page():
 
 @app.route("/search")
 def results_page():
-    u = User()
+    u = User(name="Ashleigh", logged_in=True)
     res = [[3, "Ashleigh", "Adelaide the Mercenary"],
            [1, "Eric", "Phillip the Bastard"],
            [5, "Ashleigh", "Anna the Knight"],
            [7, "Anonymous", "Mildred the Surgeon"]]
-    upstr = """
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Vivamus eu odio sit amet nibh consequat
-            consequat. Etiam et lectus nibh. Fusce quis placerat
-            libero. Aliquam interdum mattis ipsum id dictum. Donec
-            consequat scelerisque risus vel consequat. Integer eget
-            libero eleifend mauris imperdiet vehicula eu sit amet
-            erat. Aenean mauris lacus, dignissim sit amet metus
-            eget, pharetra semper purus. Donec quis viverra nulla.
-            Fusce nec urna neque. Fusce tincidunt dolor eget libero
-            mattis consectetur.
-            """
-    up = [upstr for k in range(5)]
     return render_template("search_results.html", user=u, results=res)
 
 
