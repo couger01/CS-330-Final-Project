5a3a42a98ef0edd0e27aa8bf415e5dfb22487fe1
Couger01 <couger01@luther.edu>
2017-05-16 12:17:07 -0500

    Fixes

--- character_creator_server.py
+++ character_creator_server.py
@@ -5,8 +5,8 @@ from flask_security import Security, SQLAlchemyUserDatastore, \
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, TextAreaField
-#from wtforms.validators import
-from wtforms import DataRequired, AnyOf
+from wtforms.validators import DataRequired
+from wtforms import AnyOf
 import psycopg2
 import os
 import requests as req
